1ea40012ac81d607400cb30540055f019eb66998
YYYankov <96125323+YYYankov@users.noreply.github.com>
2022-07-04 22:27:48 +0300

    Create bin1.py

---
+++ binary/bin1.py
@@ -0,0 +1,10 @@
+binary_file=open("document.bin",mode="wb+")
+text="This is good"
+encoded=text.encode("utf-8")
+binary_file.write(encoded)
+binary_file.seek(0)
+binary_data=binary_file.read(4)
+print("binary:",binary_data)
+text=binary_data.decode("utf-8")
+print("Decoded data:",text)
+binary_file.close()

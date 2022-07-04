8e75fb6959a70691da2522b22837c25464fa06a2
YYYankov <96125323+YYYankov@users.noreply.github.com>
2022-07-04 22:26:10 +0300

    Create main.py

---
+++ Geometry/main.py
@@ -0,0 +1,11 @@
+import rect, square, triangle, rhomb, trapecoid
+
+print(rect.area(int(input("a: ")), int(input("b: "))))
+
+print(square.area(int(input("a: "))))
+
+print(triangle.area(int(input("a: ")), int(input("h: "))))
+
+print(rhomb.area(int(input("a: ")), int(input("h: "))))
+
+print(trapecoid.area(int(input("a: ")), int(input("b: ")), int(input("h: "))))
